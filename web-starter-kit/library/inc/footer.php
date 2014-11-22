8c5ad8a65b6ffe02b990cf7de2af089df8aeb33b
jackbarham <jack@airboxmedia.com>
2014-11-22 14:45:12 +0000

    clear files

--- web-starter-kit/library/inc/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // Swap this line with page footer
-    include('/library/inc/footer.php');
-?>
--- web-starter-kit/library/inc/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // Swap this line with page header
-    include('/library/inc/header.php');
-?>
